12c7eb0e758aa4d1ec6f32f00250ac0eba74a914
HyungKu Im <damansa1@naver.com>
2022-11-10 12:16:40 +0900

    유효성 검사 추가

--- shopping/src/main/java/com/oraclejava/controller/PurchaseController.java
+++ shopping/src/main/java/com/oraclejava/controller/PurchaseController.java
@@ -27,17 +27,26 @@ public class PurchaseController {
 					  @RequestParam("address") String address,
 					  HttpSession session,
 					  Model model) {
+		//이름이나 주소를 입력하지 않을 경우 유효성 검사
+		if (name.isEmpty() || address.isEmpty()) {
+			model.addAttribute("contents", "purchase-out :: error-empty");
+			return "index";
+		}
+		
 		
 		//카트를 가져오자!
 		List<Item> cart = (List<Item>)session.getAttribute("cart");
 		
-		// 카트를 방치(30분이상)
+		// 카트를 방치(30분이상)한 경우 에러 메시지 표시
 		if (cart == null) {
 			model.addAttribute("contents", "purchase-out :: error-cart");
 			return "index";
 		}
 		
 		purchaseRepository.insert(cart, name, address);
+		
+		// 구입이 완료된 경우 카트를 반납
+		session.removeAttribute("cart");
 		
 		model.addAttribute("contents", "purchase-out :: purchase-out");
 		return "index";
